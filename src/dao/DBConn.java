dc6a2d73e7f9cd82cc6bc5c8a111c34320dace7c
Akemon <1404257522@qq.com>
2017-07-21 11:02:27 +0800

    dbChange

--- src/dao/DBConn.java
+++ src/dao/DBConn.java
@@ -2,11 +2,25 @@ package dao;
 import java.sql.*;
 
 public class DBConn {
+	private String url="jdbc:mysql://127.0.0.1:3306/tv_data";
+	private String driver ="com.mysql.jdbc.Driver";
+	private String user ="root";
+	private String pass ="";
+	private Connection connection ;
 	public DBConn(){
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, user, pass);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	public Connection getConnect(){
-		return null;
+		return connection;
+	}
+	public static void main(String[] args) {
+		new DBConn();
 	}
 
 }
