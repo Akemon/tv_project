30459bf59e9b25e0964f58ccdc8ccd77b8f7e0fe
Akemon <1404257522@qq.com>
2017-07-24 15:43:15 +0800

    add servlet

--- src/servlet/GetUserInfoServlet.java
+++ src/servlet/GetUserInfoServlet.java
@@ -15,12 +15,12 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import java.util.*;
 
-public class UserInfoServlet extends HttpServlet {
+public class GetUserInfoServlet extends HttpServlet {
 
 	/**
 	 * Constructor of the object.
 	 */
-	public UserInfoServlet() {
+	public GetUserInfoServlet() {
 		super();
 	}
 
@@ -47,6 +47,7 @@ public class UserInfoServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		response.setHeader("Content-Type", "text/html;charset=UTF-8");
 		response.setCharacterEncoding("UTF-8");	
+		System.out.println("½øÈëgetUserInfo....");
 		List<UserInfo> userInfoList = new UserInfoData().getUserInfoList();
 		JSONArray jsonArray =JSONArray.fromObject(userInfoList);
 		JSONObject jsonObject =new JSONObject();
